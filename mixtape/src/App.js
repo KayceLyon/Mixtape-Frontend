edd415fe50d264c5b3ecca9a13616cd26400e34a
Kayce Hubbard <ghuleh@Nameless-Ghuleh.local>
2023-01-03 10:12:14 -0600

    Created basic React app; added react-router basics; created basic components.

---
+++ mixtape/src/App.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from 'react'
+import {Route, Routes, Navigate, useSearchParams} from 'react-router-dom'
+
+import Navigation from './components/Navigation'
+import Index from './components/Index'
+import Annotation from './components/Annotation'
+import EditAnnotation from './components/EditAnnotation'
+
+const App = () => {
+return (
+  <>
+  <Routes>
+      <Route path = "/api/playlists" element={<Navigation />}>
+        <Route index element={<Index />} /> 
+        <Route path="new" element={<Annotation />} />
+        <Route path="edit/:id" element={<EditAnnotation />} />
+      </Route>
+      <Route path="/" element={<Navigate to="/api/playlists" />} />
+    </Routes>
+  </>
+)
+}
+
+export default App;
---
+++ mixtape/src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Link, Outlet} from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+const Navigation = (params) => {
+    return (
+      <>
+       </>
+  );
+}
+
+export default Navigation
