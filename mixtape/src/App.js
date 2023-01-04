5fbfeaabc908d511c19b885f3bd6db3b9f54e65c
Kayce Hubbard <ghuleh@Nameless-Ghuleh.local>
2023-01-04 07:30:49 -0600

    Set up basic routes

--- mixtape/src/App.js
+++ mixtape/src/App.js
@@ -7,13 +7,21 @@ import Annotation from './components/Annotation'
 import EditAnnotation from './components/EditAnnotation'
 
 const App = () => {
+
+  let [playlists, setPlaylists] = useState([])
+  const [formData, setFormData] = useState({
+    title: "",
+    author: "",
+    summary: ""
+  })
+
 return (
   <>
   <Routes>
       <Route path = "/api/playlists" element={<Navigation />}>
-        <Route index element={<Index />} /> 
-        <Route path="new" element={<Annotation />} />
-        <Route path="edit/:id" element={<EditAnnotation />} />
+        <Route index element={<Index playlists = {playlists} setPlaylists = {setPlaylists} />} />
+        <Route path="new" element={<Annotation formData = {formData} setFormData = {setFormData} playlists = {playlists} />} />
+        <Route path="edit/:id" element={<EditAnnotation playlists = {playlists} setPlaylists = {setPlaylists} />} />
       </Route>
       <Route path="/" element={<Navigate to="/api/playlists" />} />
     </Routes>
