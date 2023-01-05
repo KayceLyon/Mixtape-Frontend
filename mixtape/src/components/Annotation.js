690259a054a9c33149839d3a4e7c800083e76f2a
Bailey McDonald <mcdonald.bailey@gmail.com>
2023-01-05 09:21:27 -0600

    song add form coming along... inputs there, no handler function yet

--- mixtape/src/components/Annotation.js
+++ mixtape/src/components/Annotation.js
@@ -4,14 +4,15 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button'
 
 const Annotation = (params) => {
-
-  const [song, setSong] = useState([{
+  let [showSongForm, setShowSongForm] = useState(false)
+  let [songNumber, setSongNumber] = useState(0)
+  let [song, setSong] = useState([{
     "title":"",
     "artist":"",
     "cover":"",
     "album": "",
     "playlist": {
-      "id": ""
+      "id": "" ///find correct id thru params.formdata.id
     }
 }])
 
@@ -62,15 +63,42 @@ const Annotation = (params) => {
 
     return (
       <Container className='container'fluid>
+        {showSongForm === false ?
         <form onSubmit={handleNewPlaylist}>
-      <h1 >Notes</h1>
+          <h1 >Notes</h1>
           <input className="form-control origin" type='text' name='title' onChange={handleTitleChange} placeholder="Title" /><br/>
           <input className="form-control origin" type='text' name='author' onChange={handleAuthorChange} placeholder="Author" /><br/>
           <input className="form-control origin" type='text' name='summary' onChange={handleSummaryChange} placeholder="Summary" /><br/>
           
-          <input className="form-control origin" type='text' name="title" onChange={handleSongChange} placeholder="song title"/>
-          <Button variant='light' type='submit' >Add Notes</Button>
+          <Button variant='light' type='submit' onClick={()=>setShowSongForm(true)} >Add Notes</Button>
+        </form>
+        :
+         <form>
+          <h1>{params.formData.title} </h1>
+            <small>by {params.formData.author}</small><br/>
+          {songNumber === 0 ? 
+            <>
+            
+            is empty! Add your first Song!<br/>
+            Remember... the vibe is : {params.formData.summary} <br/><br/>
+            </>
+          :
+            null
+          }
+          <label>Song {songNumber + 1}</label> <br/>
+          <label> Song title</label>
+          <input className="form-control origin" type='text' name="title" onChange={handleSongChange} placeholder="title"/>
+          <label> Song artist</label>
+          <input className="form-control origin" type='text' name="artist" onChange={handleSongChange} placeholder="artist"/>
+          <label> Song cover</label><br/>
+          <small>(paste image address here)</small>
+          <input className="form-control origin" type='text' name="cover" onChange={handleSongChange} placeholder="cover"/>
+          <label> Song album</label>
+          <input className="form-control origin" type='text' name="album" onChange={handleSongChange} placeholder="album"/>
+
           </form>
+        }
+      
       </Container>
     )
 }
